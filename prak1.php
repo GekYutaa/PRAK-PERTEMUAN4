3057dbf383cbda8cd797d1bf7ee4d8cc86bb60da
Yutta <167959797+GekYutaa@users.noreply.github.com>
2024-05-03 15:29:44 +0800

    Upload praktikum pertemuan ke 4

---
+++ prak1.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>praktikum 1</title>
+</head>
+<body>
+    <?php
+    echo "<h1>INSTITUT BISNIS DAN TEKNOLOGI INDONESIA</h1>";
+    echo "Program PHP Pertamaku<br>";
+    ?>
+</body>
+</html>
